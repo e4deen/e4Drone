91c5e2176b9fab6fad6cd9a7012c8e337f5e40bf
Raven <craven@crowz.kr>
2016-07-09 17:17:54 +0900

    Commit Default file structor

---
+++ src/Control/FlightControl/FlightControl.h
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2016 "e4Deen,"
+ *
+ * This file is part of e4Drone.
+ *
+ * e4Drone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * e4Drone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with e4Drone.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @file 	FlightControl.h
+ * @date 	2016. 7. 9.
+ * @author	
+ * @brief
+ */
+#ifndef CONTROL_FLIGHTCONTROL_FLIGHTCONTROL_H_
+#define CONTROL_FLIGHTCONTROL_FLIGHTCONTROL_H_
+
+
+
+
+
+#endif /* CONTROL_FLIGHTCONTROL_FLIGHTCONTROL_H_ */
